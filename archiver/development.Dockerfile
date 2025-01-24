a1c8a5fdca967f13f2ebc67871ffe8c28d042986
Michael Johnson <michael@icefoganalytics.com>
2025-01-24 14:12:25 -0700

    Archiver basic application

--- archiver/development.Dockerfile
+++ archiver/development.Dockerfile
@@ -1,5 +1,22 @@
 FROM node:20.18.0-alpine3.19
 
+# Install Java (OpenJDK)
+RUN apk add --no-cache openjdk11
+
+# Set JAVA_HOME environment variable
+ENV JAVA_HOME="/usr/lib/jvm/java-11-openjdk"
+ENV PATH="$JAVA_HOME/bin:$PATH"
+
+# Install iText SDK (you'll need to have the iText JAR file or download it)
+# Here, I’m assuming you have the JAR file available locally or at a specific URL
+
+# COPY or ADD the iText JAR file
+# For example, if it's local:
+COPY path/to/itextpdf-7.x.x.jar /app/libs/
+
+# If it's from a URL, you can use wget or curl
+# RUN wget -O /app/libs/itextpdf-7.x.x.jar http://example.com/itextpdf-7.x.x.jar
+
 RUN npm install -g npm@10.9.0
 
 WORKDIR /usr/src/api
---
+++ archiver/src/jobs/pdf-converter-job.ts
@@ -0,0 +1,55 @@
+import cache from "@/db/cache-client"
+import logger from "@/utils/logger"
+import { FileStorageService } from "@/services"
+import { isNil } from "lodash"
+import { ArchiveItemFile } from "@/models"
+
+export class PDFConverterJob {
+  name = "pdf-converter"
+  schedule = "*/1 * * * *"
+
+  constructor() {}
+
+  async run(statDate: Date) {
+    logger.info("Running PDF Converter Job", statDate)
+    const toConvert = await cache.getKeysByPattern(`CONVERT_`)
+    const fileStore = new FileStorageService()
+
+    for (const key of toConvert) {
+      const data = await cache.getValue(key)
+
+      if (isNil(data)) return
+
+      const fileInfo = JSON.parse(data)
+      const file = await fileStore.downloadFile(fileInfo.originalKey)
+      const fileRecord = await ArchiveItemFile.findBySlugOrPk(fileInfo.id)
+      if (!fileRecord) return
+
+      const folderKey = fileInfo.originalKey.substring(0, fileInfo.originalKey.indexOf("/"))
+      const convertedPdfKey = `${folderKey}/${fileStore.makeKey()}`
+
+      console.log("DOWNLOADED FILE", file.length)
+
+      //TODO: This is where the magical conversion and signing happens
+      // the converted file should be called `convertedAndSignedFile`
+      // it can be saved to a local directory for processing if required.
+
+
+      // after the conversion is complete, upload the file to the file store, update the file record and delete the cache key
+      /* 
+      const uploadResp = await fileStore.uploadFile(convertedPdfKey, convertedAndSignedFile)
+
+      if (uploadResp.errorCode) {
+        throw Error("File upload error")
+      }
+      
+      fileRecord.pdfKey = convertedPdfKey
+      fileRecord.pdfFileName = `${fileRecord.originalFileName}_SIGNED.pdf`
+      fileRecord.pdfMimeType = "application/pdf"
+      fileRecord.pdfFileSize = convertedAndSignedFile.length
+      await fileRecord.update()
+
+      cache.deleteValue(key) */
+    }
+  }
+}
