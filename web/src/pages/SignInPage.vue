aa571627002029c36e9b9c19cda1d77a1b19f8b8
Michael Johnson <michael@icefoganalytics.com>
2025-02-02 12:09:49 -0700

    Better sign-in design

--- web/src/pages/SignInPage.vue
+++ web/src/pages/SignInPage.vue
@@ -1,48 +1,60 @@
 <template>
-  <div class="pa-3">
-    <v-row class="h-100vh mh-100 auth">
+  <div class="fill-height">
+    <v-row
+      class="h-100vh mh-100 auth my-0"
+      :class="{ 'fill-height': !mdAndDown }"
+    >
       <v-col
         cols="12"
-        lg="7"
-        xl="8"
-        class="d-lg-flex align-center justify-center authentication position-relative"
+        lg="8"
+        xl="9"
+        class="d-lg-flex align-center justify-center"
+        style="overflow: hidden"
       >
-        <img
-          src="@/assets/SplashImage.png"
-          class="position-relative d-none d-lg-flex"
-          alt="login-background"
-        />
+        <v-icon
+          class="d-none d-lg-flex"
+          color="primary"
+          size="400"
+          style="opacity: 0.05"
+          >mdi-shield-key</v-icon
+        >
       </v-col>
       <v-col
         cols="12"
-        class="d-flex d-lg-none align-center text-center justify-center bg-surface"
-      >
-        <img
-          src="@/assets/SplashImage.png"
-          style="width: 50%; max-width: 200px"
-          alt="login-background"
-        />
-      </v-col>
-      <v-col
-        cols="12"
-        lg="5"
-        xl="4"
-        class="d-flex align-center justify-center bg-surface"
+        lg="4"
+        xl="3"
+        class="d-flex align-center justify-center pl-0 bg-surface"
       >
         <div
-          class="mt-xl-0 mt-5 mw-100 text-center"
-          text-center
+          class="d-flex align-center justify-center py-5"
+          style="
+            width: 100%;
+            background-color: #ddd;
+            border-top: 10px #aaa solid;
+            border-bottom: 10px #aaa solid;
+          "
         >
-          <h2 class="text-h3 font-weight-semibold mb-2">{{ APPLICATION_NAME }}</h2>
-          <div class="text-subtitle-1 mb-6 mt-3 font-weight-bold">Yukon Government</div>
-          <div class="my-6 text-center">
-            <v-btn
-              color="primary"
-              @click="loginWithRedirect"
-            >
-              Sign in
-            </v-btn>
-            <div class="text-subtitle-1 mt-2">Using your YNET Credentials</div>
+          <v-icon
+            v-if="mdAndDown"
+            color="primary"
+            :size="smAndUp ? 200 : 100"
+            style="opacity: 0.65"
+            >mdi-shield-key</v-icon
+          >
+          <div class="text-center">
+            <h2 class="text-h3 font-weight-semibold mb-2">{{ APPLICATION_NAME }}</h2>
+            <div class="text-subtitle-1 mb-6 mt-n3 font-weight-bold">Yukon Government</div>
+            <div class="mt-6 text-center">
+              <v-btn
+                color="primary"
+                @click="loginWithRedirect"
+              >
+                Sign in
+              </v-btn>
+              <div class="text-subtitle-1 mt-5">Using your YNET Credentials</div>
+              <v-divider class="my-2" />
+              <em style="font-weight: 700">Secure Digital Storage</em>
+            </div>
           </div>
         </div>
       </v-col>
@@ -53,9 +65,12 @@
 <script lang="ts" setup>
 import { onMounted } from "vue"
 import { useAuth0 } from "@auth0/auth0-vue"
+import { useDisplay } from "vuetify/lib/framework.mjs"
 
 import { APPLICATION_NAME } from "@/config"
 import useCurrentUser from "@/use/use-current-user"
+
+const { mdAndDown, smAndUp } = useDisplay()
 
 const { reset: resetCurrentUser } = useCurrentUser()
 
